53bf4573a997a0ae16b5a75be69a3c16ea4b0c8a
OpenTech-Consult <opentech.consult@gmail.com>
2022-12-07 05:32:05 +0000

    TypeScript Basic Data Types

---
+++ lesson02/build/js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+var myName = 'Sandro';
+var meaningOfLife;
+var isLoading;
+var album;
+myName = 'Alessandro';
+meaningOfLife = 42;
+isLoading = true;
+album = 5150;
+var sum = function (a, b) {
+    return a + b;
+};
+var postId;
+var isActive;
+var re = /\w+/g;
