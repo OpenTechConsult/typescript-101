8178ddff36a2e77e6127141f19e16af20f3516a9
OpenTech-Consult <opentech.consult@gmail.com>
2022-12-07 19:35:09 +0000

    TypeScript
    Classes.
    
    Created a class with constructor and access modifier.
    
    Instantiated the class created

---
+++ lesson06/build/js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+var Coder = /** @class */ (function () {
+    function Coder(name, music, age, lang) {
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+    }
+    return Coder;
+}());
+var Dave = new Coder('Dave', 'Rock', 42, 'Typescript');
---
+++ lesson06/src/main.ts
@@ -0,0 +1,18 @@
+class Coder {
+
+    secondLang!: string
+    
+    constructor(
+        public readonly name: string, 
+        public music: string, 
+        private age: number, 
+        protected lang: string
+    ) {
+        this.name = name
+        this.music = music
+        this.age = age
+        this.lang = lang
+    }
+}
+
+const Dave = new Coder('Dave', 'Rock', 42, 'Typescript')
